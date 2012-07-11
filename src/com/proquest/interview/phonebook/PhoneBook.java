6a051c59153f570a3859b42ee6b1561c9b6e5a55
riteshbasatwar <ritesh.basatwar@gmail.com>
2012-07-11 00:40:44 -0500

    Trying to solve for interview

--- src/com/proquest/interview/phonebook/PhoneBook.java
+++ src/com/proquest/interview/phonebook/PhoneBook.java
@@ -1,6 +1,17 @@
 package com.proquest.interview.phonebook;
 
+import java.util.ArrayList;
+
+/**
+ * Interface declaring methods that needs to be implemented for PhoneBook
+ */
+
 public interface PhoneBook {
-	public Person findPerson(String firstName, String lastName);
+    public ArrayList findPersonByFirstName(String firstName);
+    public ArrayList findPersonByLastName(String lastName);
+    public ArrayList findPersonByPhoneNumber(String phoneNo);
+    public ArrayList findPersonByAddress(String address);
 	public void addPerson(Person newPerson);
+    public Person findPerson(String firstName, String lastName);
+    public void printPhoneBook();
 }
